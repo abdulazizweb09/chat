1a3c314e11219fee148ea12e0501f82c4430764d
abdulazizweb09 <xakimovxadyatillo92@gmail.com>
2025-07-09 09:43:38 +0500

    refactor: rename page component to Page and clean up formatting

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,17 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 
-function page() {
+function Page() {
   let router = useRouter()
   function to() {
     router.push("/register");
   }
-  
+
   return (
     <div>
-      <button className=" border mx-auto container cursor-pointer h-20" onClick={to}>register</button>
+      <button className="border mx-auto container cursor-pointer h-20" onClick={to}>register</button>
     </div>
   );
 }
 
-export default page;
+export default Page;
